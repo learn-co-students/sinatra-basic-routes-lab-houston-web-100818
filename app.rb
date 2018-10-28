419d47bfd472f6e5ff6b0fdad8e52560bcedd9e1
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-10-28 13:39:02 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Bebe"
+    end
+
+    get '/hometown' do
+        "My hometown is Nigeria"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is IDOL from BTS"
+    end
 end
