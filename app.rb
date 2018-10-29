e8d24af222a5fc75671affb66d69c85935e850d2
barbaradecares <barbaradecares@hotmail.com>
2018-10-29 14:13:00 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Barbara"
+    end 
+
+    get '/hometown' do
+        "My hometown is Santos"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Vai Malandra"
+    end 
 end
+ 
